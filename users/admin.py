e2f516e6a9addb73a0046357297694beddbbf515
paullee714 <ba93love@gmail.com>
2019-12-21 13:23:58 +0900

    setting aribnb user Model, Migrate, createsuperuser

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+
+    """ CUSTOM USER ADMIN """
+
+    # list_display = (
+    #     "username",
+    #     "email",
+    #     "gender",
+    #     "language",
+    #     "currency",
+    #     "superhost",
+    # )
+    # list_filter = ("superhost", "language", "currency")
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "language",
+                    "birthdate",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
