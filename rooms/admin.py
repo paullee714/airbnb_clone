cf1195b2bf1a48ff74827b95bebe11480c620b1c
paullee714 <ba93love@gmail.com>
2019-12-23 14:40:09 +0900

    Room Admin set Custom Admin Panel

--- rooms/admin.py
+++ rooms/admin.py
@@ -15,7 +15,53 @@ class ItemAdmin(admin.ModelAdmin):
 class RoomAdmin(admin.ModelAdmin):
     """Room Admin Definition"""
 
-    pass
+    # fieldssets는 admin의 추가 화면들을 사용자가 꾸밀 수 있다.
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facility", "house_rules"),
+            },
+        ),
+        ("Last Details", {"fields": ("hosts",)}),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "address",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    list_filter = (
+        "instant_book",
+        "hosts__superhost",
+        "room_type",
+        "amenities",
+        "facility",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    search_fields = ("city", "hosts__username")
+
+    filter_horizontal = ("amenities", "facility", "house_rules")
 
 
 @admin.register(models.Photo)
