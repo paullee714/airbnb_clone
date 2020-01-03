46c167b094bc055a0ea8c337da029f69fc2cbf7c
paullee714 <ba93love@gmail.com>
2020-01-03 18:27:11 +0900

    function based view -> class based view

--- rooms/views.py
+++ rooms/views.py
@@ -1,17 +1,33 @@
-from math import ceil
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator
+# from math import ceil
+# from django.shortcuts import render, redirect
+# from django.core.paginator import Paginator
 from . import models
+from django.views.generic import ListView
+from django.utils import timezone
 
+# 함수형 뷰
+# def all_rooms(request):
+#     page = request.GET.get("page", 1)
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
+#     # paginator사용하기
+#     room_list = models.Room.objects.all()
+#     paginator = Paginator(room_list, 10, orphans=5)
+#     try:
+#         rooms = paginator.get_page(int(page))
+#         return render(request, "rooms/home.html", {"page": rooms})
+#     except EmptyPage:
+#         return redirect("/")
 
-    # paginator사용하기
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-    try:
-        rooms = paginator.get_page(int(page))
-        return render(request, "rooms/home.html", {"page": rooms})
-    except EmptyPage:
-        return redirect("/")
+# 클래스 뷰
+class HomeView(ListView):
+    """ HomeView Definition"""
+
+    model = models.Room
+    paginate_by = 10
+    ordering = "created"
+    paginate_orphans = 5
+    context_object_name = "rooms"
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     return context
