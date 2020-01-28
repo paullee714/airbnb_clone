4b673a8b40f76974f7d75272a9796e95cdc2ff06
paullee714 <ba93love@gmail.com>
2020-01-28 13:41:14 +0900

    using UserCreationForm in signup page

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from . import models
+from django.contrib.auth.forms import UserCreationForm
 
 
 class LoginForm(forms.Form):
@@ -60,27 +61,31 @@ class LoginForm(forms.Form):
 
 ################
 ##SignUp Form to ModelForm
-class SignUpForm(forms.ModelForm):
-    class Meta:
-        model = models.User
-        fields = ("first_name", "last_name", "email")
+# class SignUpForm(forms.ModelForm):
+#     class Meta:
+#         model = models.User
+#         fields = ("first_name", "last_name", "email")
 
-    password = forms.CharField(widget=forms.PasswordInput)
-    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+#     password = forms.CharField(widget=forms.PasswordInput)
+#     password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
 
-    def clean_password1(self):
-        password = self.cleaned_data.get("password")
-        password1 = self.cleaned_data.get("password1")
+#     def clean_password1(self):
+#         password = self.cleaned_data.get("password")
+#         password1 = self.cleaned_data.get("password1")
 
-        if password != password1:
-            raise forms.ValidationError("Password confirmation does not match")
-        else:
-            return password
+#         if password != password1:
+#             raise forms.ValidationError("Password confirmation does not match")
+#         else:
+#             return password
 
-    def save(self, *args, **kwargs):
-        user = super().save(commit=False)
-        email = self.cleaned_data.get("email")
-        password = self.cleaned_data.get("password")
-        user.username = email
-        user.set_password(password)
-        user.save()
+#     def save(self, *args, **kwargs):
+#         user = super().save(commit=False)
+#         email = self.cleaned_data.get("email")
+#         password = self.cleaned_data.get("password")
+#         user.username = email
+#         user.set_password(password)
+#         user.save()
+
+##SignUp Form -- UserCreationForm
+class SignUpForm(UserCreationForm):
+    username = forms.EmailField(label="Email")
