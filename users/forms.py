597bba44baccad6e33eb6cbd3b15ba3ffdb05396
paullee714 <ba93love@gmail.com>
2020-01-29 13:50:49 +0900

    Login form Placeholder 주기, UserCreationForm 재작성

--- users/forms.py
+++ users/forms.py
@@ -5,8 +5,10 @@ from django.contrib.auth.forms import UserCreationForm
 
 class LoginForm(forms.Form):
 
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder": "Email"}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+    )
 
     def clean(self):
         email = self.cleaned_data.get("email")
@@ -61,7 +63,34 @@ class LoginForm(forms.Form):
 
 ################
 ##SignUp Form to ModelForm
-class SignUpForm(forms.ModelForm):
+# class SignUpForm(forms.ModelForm):
+#     class Meta:
+#         model = models.User
+#         fields = ("first_name", "last_name", "email")
+
+#     password = forms.CharField(widget=forms.PasswordInput)
+#     password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+#     def clean_password1(self):
+#         password = self.cleaned_data.get("password")
+#         password1 = self.cleaned_data.get("password1")
+
+#         if password != password1:
+#             raise forms.ValidationError("Password confirmation does not match")
+#         else:
+#             return password
+
+#     def save(self, *args, **kwargs):
+#         user = super().save(commit=False)
+#         email = self.cleaned_data.get("email")
+#         password = self.cleaned_data.get("password")
+#         user.username = email
+#         user.set_password(password)
+#         user.save()
+
+
+##SignUp Form -- UserCreationForm
+class SignUpForm(UserCreationForm):
     class Meta:
         model = models.User
         fields = ("first_name", "last_name", "email")
@@ -85,8 +114,3 @@ class SignUpForm(forms.ModelForm):
         user.username = email
         user.set_password(password)
         user.save()
-
-
-##SignUp Form -- UserCreationForm
-# class SignUpForm(UserCreationForm):
-#     username = forms.EmailField(label="Email")
