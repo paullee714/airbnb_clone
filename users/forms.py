f6e27fc75e04c23600f25b44734c06b6e122fe13
paullee714 <ba93love@gmail.com>
2020-01-29 14:12:01 +0900

    다시 modelform....

--- users/forms.py
+++ users/forms.py
@@ -63,40 +63,23 @@ class LoginForm(forms.Form):
 
 ################
 ##SignUp Form to ModelForm
-# class SignUpForm(forms.ModelForm):
-#     class Meta:
-#         model = models.User
-#         fields = ("first_name", "last_name", "email")
-
-#     password = forms.CharField(widget=forms.PasswordInput)
-#     password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
-
-#     def clean_password1(self):
-#         password = self.cleaned_data.get("password")
-#         password1 = self.cleaned_data.get("password1")
-
-#         if password != password1:
-#             raise forms.ValidationError("Password confirmation does not match")
-#         else:
-#             return password
-
-#     def save(self, *args, **kwargs):
-#         user = super().save(commit=False)
-#         email = self.cleaned_data.get("email")
-#         password = self.cleaned_data.get("password")
-#         user.username = email
-#         user.set_password(password)
-#         user.save()
-
-
-##SignUp Form -- UserCreationForm
-class SignUpForm(UserCreationForm):
+class SignUpForm(forms.ModelForm):
     class Meta:
         model = models.User
         fields = ("first_name", "last_name", "email")
+        widgets = {
+            "first_name": forms.TextInput(attrs={"placeholder": "First Name"}),
+            "last_name": forms.TextInput(attrs={"placeholder": "Last Name"}),
+            "email": forms.EmailInput(attrs={"placeholder": "Email"}),
+        }
 
-    password = forms.CharField(widget=forms.PasswordInput)
-    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"}),
+        # label="Confirm Password",
+    )
 
     def clean_password1(self):
         password = self.cleaned_data.get("password")
@@ -114,3 +97,40 @@ class SignUpForm(UserCreationForm):
         user.username = email
         user.set_password(password)
         user.save()
+
+
+##SignUp Form -- UserCreationForm
+# class SignUpForm(UserCreationForm):
+#     class Meta:
+#         model = models.User
+#         fields = ("first_name", "last_name", "email")
+#         widgets = {
+#             "first_name": forms.TextInput(attrs={"placeholder": "First Name"}),
+#             "last_name": forms.TextInput(attrs={"placeholder": "Last Name"}),
+#             "email": forms.EmailInput(attrs={"placeholder": "Email"}),
+#         }
+
+#     password = forms.CharField(
+#         widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+#     )
+#     password1 = forms.CharField(
+#         widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"}),
+#         # label="Confirm Password",
+#     )
+
+#     def clean_password1(self):
+#         password = self.cleaned_data.get("password")
+#         password1 = self.cleaned_data.get("password1")
+
+#         if password != password1:
+#             raise forms.ValidationError("Password confirmation does not match")
+#         else:
+#             return password
+
+#     def save(self, *args, **kwargs):
+#         user = super().save(commit=False)
+#         email = self.cleaned_data.get("email")
+#         password = self.cleaned_data.get("password")
+#         user.username = email
+#         user.set_password(password)
+#         user.save()
